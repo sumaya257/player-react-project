1db601d273febac74eb0505af14c0cdf7b352b5c
sumaya257 <sumayaece19@gmai.com>
2024-10-25 08:21:17 +0600

    toggoling-btn-completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,33 @@ import './App.css';
 import Banner from './assets/components/Banner';
 import Navbar from './assets/components/Navbar';
 import BtnContainer from './assets/components/BtnContainer/BtnContainer';
-import Available from './assets/components/Available/Available';
+
 
 function App() {
   const [amount, setAmount] = useState(0);
-
-  //btn toggling
-  const[isActive, setIsActive] = useState({
+  
+  const [isActive, setIsActive] = useState({
     Available: true,
-    status:'active'
-})
-// console.log(isActive)
+    status: 'active'
+  });
 
-const handleFreeCredit = () => {
-    setAmount(amount + 600000)
-    console.log('got',amount + 600000)
+  const handleFreeCredit = () => {
+    setAmount(amount + 600000);
+    console.log('got', amount + 600000);
   };
 
-  //btn toggling handler
-  
-  
+  const handleIsActiveState = (status) => {
+    setIsActive({
+      Available: status === 'Available',
+      status: status === 'Available' ? 'active' : 'inactive'
+    });
+  };
+console.log(isActive)
   return (
     <div className='container mx-auto px-4'>
       <Navbar amount={amount} />
       <Banner handleFreeCredit={handleFreeCredit} />
-      <BtnContainer></BtnContainer>
+      <BtnContainer isActive={isActive} handleIsActiveState={handleIsActiveState} />
     </div>
   );
 }
--- src/assets/components/BtnContainer/BtnContainer.jsx
+++ src/assets/components/BtnContainer/BtnContainer.jsx
@@ -1,14 +1,32 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './BtnContainer.css'
+import Available from '../Available/Available';
+import Selected from '../Selected/Selected';
 
-const BtnContainer = () => {
-    return (
-        <div className='flex justify-end'>
-            <div><button className='btn'>Available</button></div>
-            <div><button className='btn'>Selected</button></div>
-        </div>
-    );
+const BtnContainer = ({ handleIsActiveState, isActive }) => {
+  return (
+    <div>
+    <div className="flex justify-end mb-4">
+      <button
+        onClick={() => handleIsActiveState('Available')}
+        className={`btn ${isActive.Available ? 'active' : 'inactive'}`}>
+        Available
+      </button>
+
+      <button
+        onClick={() => handleIsActiveState('Selected')}
+        className={`btn ${!isActive.Available ? 'active' : 'inactive'} ml-2`}>
+        Selected
+      </button>
+    </div>
+
+   {isActive.Available?<Available></Available>:<Selected></Selected>}
+    </div>
+
+
+  );
 };
 
 export default BtnContainer;
